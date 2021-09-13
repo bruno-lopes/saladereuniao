6a46a8960d42fedb49f77189635678fc0aa8d58b
Bruno Lorenço Lopes <bruno.lorenco.lopes@gmail.com>
2021-09-13 14:53:15 -0300

    Criar métodos no RoomListComponent e RoomListService

--- frontend/src/app/room-list/room-list.component.ts
+++ frontend/src/app/room-list/room-list.component.ts
@@ -1,4 +1,9 @@
+import { RoomDetailsComponent } from './../room-details/room-details.component';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-list',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private roomService: RoomService, private router: Router) {
+      this.rooms = new Observable<Room[]>();
+
+   }
+
+
+  rooms: Observable<Room[]>;
 
   ngOnInit(): void {
+    this.reloadData();
   }
+
+  reloadData() {
+    this.rooms = this.roomService.getRoomList();
+
+  }
+
+  deleteRooms(id: number): void {
+    this.roomService.deleteRoom(id).subscribe(
+      data=> {
+        console.log(data);
+        this.reloadData();
+      },
+      error => console.log(error)
+    );
+  }
+
+  roomDetails(id: number): void {
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number) {
+    this.router.navigate(['update', id]);
+  }
+
+
 
 }
--- frontend/src/app/room.service.ts
+++ frontend/src/app/room.service.ts
@@ -1,9 +1,17 @@
+import { Room } from './room';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
+  deleteRoom(id: number): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  getRoomList(): Observable<Room[]> {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
